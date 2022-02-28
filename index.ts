7ffbb26897346506b7253e46047252f9be0a34d8
Johnny Moreira <johnnymoreira91@hotmail.com>
2022-02-28 20:41:55 -0300

    add media function

--- index.ts
+++ index.ts
@@ -1,6 +1,36 @@
-import moment from 'moment-timezone'
+import moment from './modules/moment-timezone'
+import { fluxo_infracao } from './interface/fluxo_infracao'
 
-export function ReduceQtdePassagemDia (array) {
+export { fluxo_infracao }
+
+export async function MediaDWY(array) {
+  const data = array
+  const total = ReduceQtdePassagemDia(data)
+  const media = getMedia(data.length, total)
+  return media
+}
+
+export async function MediaDay(array) {
+  const data = ChangeDate(array)
+  const dateArray = []
+  let mapDates = data.map(obj => obj.data)
+  mapDates = [...new Set(mapDates)]
+  for (const date of mapDates) {
+    const filterDay = data.filter(obj => obj.data === date)
+    const reduce = ReduceQtdePassagemDia(filterDay)
+    let media = getMedia(filterDay.length, reduce)
+    media = Math.round(media * 100) / 100
+    const obj = {
+      date: date,
+      total_passage_day: reduce,
+      media_day: media
+    }
+    dateArray.push(obj)
+  }
+  return dateArray
+}
+
+export function ReduceQtdePassagemDia(array) {
   const passagens = []
   for (const obj of array) {
     passagens.push(obj.qtde_passagem_dia)
@@ -9,7 +39,7 @@ export function ReduceQtdePassagemDia (array) {
   return reduceArray
 }
 
-export function ChangeDate (array) {
+export function ChangeDate(array) {
   const data = array
   const alteredData = []
   for (const obj of data) {
@@ -19,7 +49,7 @@ export function ChangeDate (array) {
   return alteredData
 }
 
-export function getMedia (total, value) {
+export function getMedia(total, value) {
   const a = total
   const b = value
   const media = value / total
